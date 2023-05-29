a2e259af51fe99472011b41a9915a6cbf4b43dae
zeeshan <zeeshanweb.official@gmail.com>
2023-05-30 04:04:03 +0500

    ADDED FIREBASE

---
+++ firebaase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_QiVLgVaHLWB0gL7fdbIWKyH-bVd66P4",
+    authDomain: "fir-test-c452d.firebaseapp.com",
+    databaseURL: "https://fir-test-c452d-default-rtdb.firebaseio.com",
+    projectId: "fir-test-c452d",
+    storageBucket: "fir-test-c452d.appspot.com",
+    messagingSenderId: "433106979752",
+    appId: "1:433106979752:web:44239afa519fdfcfaf2b9d"
+  };
+
+  
